4a6f91eb2cb101d00294ab0281996a3a1659b1fe
Luiza <luizaschettinii@gmail.com>
2025-03-18 09:37:10 -0300

    feat: create model and controller getIngressoById

--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -2,3 +2,8 @@ const getIngressos = async () => {
     const result = await pool.query("SELECT * FROM ingressos");
     return result.rows;
 };
+
+const getIngressoById = async (id) => {
+    const result = await pool.query("SELECT * FROM ingressos WHERE id = $1", [id]);
+    return result.rows[0];
+};
--- src/models/ticketModel.js
+++ src/models/ticketModel.js
@@ -3,7 +3,20 @@ const getAllIngressos = async (req, res) => {
         const ingressos = await ticketModel.getIngressos();
         res.json(ingressos);
     } catch (error) {
-        res.status(404).json({ message: "Erro ao buscar ingresso(s)." });
+        res.status(404).json({ message: "Erro ao buscar ingresso(s)." 
+        });
+    }
+};
+
+const getIngresso = async (req, res) => {
+    try {
+        const ingresso = await ticketModel.getIngressoById(req.params.id);
+        if (!ingresso) {
+            return res.status(404).json({ message: "Ingresso(s) não encontrado." });
+        }
+        res.json(ingresso);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar ingresso(s)." });
     }
 };
 
